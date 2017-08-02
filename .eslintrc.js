8c29ea637a17e0589dd4542d8fa4811adb4056ec
tobi.dara <tobi_daramola@yahoo.com>
2017-08-02 11:35:32 +0100

    Deal with errors in eslint confiiguration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,64 @@
 module.exports = {
+
     "extends": "airbnb",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+
     "env": {
-        "es6": true
+        "es6": true,
+        "browser": true,
+        "node": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
+    "plugins": [
+        "react"
+    ],
     "parserOptions": {
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
     },
     "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/jsx-uses-vars": 1,
+        "react/react-in-jsx-scope": 1,
+        "no-console": 0,
         "indent": [
-            "error",
-            "tab"
+            "error", 4
         ],
         "linebreak-style": [
             "error",
-            "unix"
+            "windows"
         ],
         "quotes": [
-            "error",
-            "double"
+            "warn",
+            "single"
         ],
         "semi": [
-            "error",
+            "warn",
             "always"
+        ],
+        "comma-dangle": [
+            "error",
+            "never"
+        ],
+        "quote-props": [
+            "off"
+        ],
+        "import/no-extraneous-dependencies": [
+            "off"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ],
+        "react/jsx-indent": [
+            "error",
+            4
         ]
     }
 };

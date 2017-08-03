15e5f499c5f32df0d541beab547dc01699144663
tobi.dara <tobi_daramola@yahoo.com>
2017-08-03 07:57:41 +0100

    create database and configure host, username and password in config.json

--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,15 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
-var Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/..\config\config.json')[env];
 var db = {};
+
+let sequelize;
 if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }

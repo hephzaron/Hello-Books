32a6aeb1f6f9e636927993135cd6ceeedbdd9dd7
tobi.dara <tobi_daramola@yahoo.com>
2017-08-03 22:00:58 +0100

    modificaton to controller

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const booksController = require('..../controllers').books;
+const signUpController = require('..../controllers').user;
+const signInController = require('..../controllers').user();
+
+module.exports = (app) => {
+
+    app.get('/api', (req, res) => res.status(200).send({
+        message: 'add books'
+    }));
+
+    app.post('/api/books', booksController.create);
+    app.put('/api/books/bookId', booksController.update);
+    app.get('/api/books', booksController.list);
+    app.post('/api/books/:bookId/list', booksController.create);
+    app.post('/api/users/signup', signUpController.create);
+    app.post('/api/users/signup', signInController.create);
+};

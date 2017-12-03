502146fbf8cacf57e102929f90880d0a746254ca
tobi.dara <tobi_daramola@yahoo.com>
2017-12-03 04:30:27 +0100

    add facebook missing authentication route

--- server/routes/index.js
+++ server/routes/index.js
@@ -48,6 +48,19 @@ module.exports = (app, passport) => {
             failureRedirect: '/'
         }));
 
+    // =====================================
+    // FACEBOOK ROUTES =======================
+    // =====================================
+
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+    // the callback after facebook has authenticated the user
+    app.get('auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+
 
 
 

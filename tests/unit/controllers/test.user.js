cc71bc405ab1cab461208ab819cae543b571f39d
tobi.dara <tobi_daramola@yahoo.com>
2018-01-03 14:21:13 +0100

    remove usercontroller sign-in unit test

--- tests/unit/controllers/test.user.js
+++ tests/unit/controllers/test.user.js
@@ -74,133 +74,134 @@ describe('REGISTER User', () => {
 });
 
 describe('SIGN-IN User', () => {
-            it('it should not sign in user for incorrect username', (done) => {
-                let request = httpMocks.createRequest({
-                    method: 'POST',
-                    params: {},
-                    body: {
-                        username: localUser[1].username,
-                        password: localUser[0].setPassword
-                    }
-                });
-                let response = httpMocks.createResponse({
-                    eventEmitter: EventEmitter
-                });
-                response.on('send', () => {
-                    try {
-                        assert.equal(response._getStatusCode(), 404);
-                        assert.equal(response._getData(), 'incorrect username or password');
-                        done();
-                    } catch (e) { console.log(e); }
-                });
-                userController.signIn(request, response, () => {}); // next() is not called
+    it('it should not sign in user for incorrect username', (done) => {
+        let request = httpMocks.createRequest({
+            method: 'POST',
+            params: {},
+            body: {
+                username: localUser[1].username,
+                password: localUser[0].setPassword
+            }
+        });
+        let response = httpMocks.createResponse({
+            eventEmitter: EventEmitter
+        });
+        response.on('send', () => {
+            try {
+                assert.equal(response._getStatusCode(), 404);
+                assert.equal(response._getData(), 'incorrect username or password');
+                done();
+            } catch (e) { console.log(e); }
+        });
+        userController.signIn(request, response, () => {}); // next() is not called
+    });
+
+    it('it should not sign in user for incorrect password', (done) => {
+        let request = httpMocks.createRequest({
+            method: 'POST',
+            params: {},
+            body: {
+                username: localUser[0].username,
+                password: localUser[1].setPassword
+            }
+        });
+        let response = httpMocks.createResponse({
+            eventEmitter: EventEmitter
+        });
+        response.on('send', () => {
+            try {
+                assert.equal(response._getStatusCode(), 404);
+                assert.equal(response._getData(), 'incorrect username or password');
+                done();
+            } catch (e) { console.log(e); }
+        });
+        userController.signIn(request, response, () => {}); // next() is not called
+    });
+    // where password field is empty request for it and not sign in user
+    it('it should request for password if empty and not sign in user', (done) => {
+        let request = httpMocks.createRequest({
+            method: 'POST',
+            params: {},
+            body: {
+                username: localUser[0].username
+            }
+        });
+        let response = httpMocks.createResponse({
+            eventEmitter: EventEmitter
+        });
+        response.on('send', () => {
+            try {
+                assert.equal(response._getStatusCode(), 404);
+                assert.equal(response._getData(), 'incorrect username or password');
+                done();
+            } catch (e) { console.log(e); }
+        });
+        userController.signIn(request, response, () => {}); // next() is not called
+    });
+
+    describe('FETCH User Books', () => {
+        it('it should fetch all users and book borrowed', (done) => {
+
+            let request = httpMocks.createRequest({
+                method: 'GET',
+                params: {},
+                body: {}
+
+            });
+            let response = httpMocks.createResponse({
+                eventEmitter: EventEmitter
             });
 
-            it('it should not sign in user for incorrect password', (done) => {
-                let request = httpMocks.createRequest({
-                    method: 'POST',
-                    params: {},
-                    body: {
-                        username: localUser[0].username,
-                        password: localUser[1].setPassword
-                    }
+            function loop(id) {
+                return borrowData.filter(function(data) {
+                    return data.userId === id;
                 });
-                let response = httpMocks.createResponse({
-                    eventEmitter: EventEmitter
-                });
-                response.on('send', () => {
-                    try {
-                        assert.equal(response._getStatusCode(), 404);
-                        assert.equal(response._getData(), 'incorrect username or password');
-                        done();
-                    } catch (e) { console.log(e); }
-                });
-                userController.signIn(request, response, () => {}); // next() is not called
-            });
-            // where password field is empty request for it and not sign in user
-            it('it should request for password if empty and not sign in user', (done) => {
-                let request = httpMocks.createRequest({
-                    method: 'POST',
-                    params: {},
-                    body: {
-                        username: localUser[0].username
-                    }
-                });
-                let response = httpMocks.createResponse({
-                    eventEmitter: EventEmitter
-                });
-                response.on('send', () => {
-                    try {
-                        assert.equal(response._getStatusCode(), 404);
-                        assert.equal(response._getData(), 'incorrect username or password');
-                        done();
-                    } catch (e) { console.log(e); }
-                });
-                userController.signIn(request, response, () => {}); // next() is not called
-            });
+            }
 
-            describe('FETCH User Books', () => {
-                it('it should fetch all users and book borrowed', (done) => {
-
-                    let request = httpMocks.createRequest({
-                        method: 'GET',
-                        params: {},
-                        body: {}
-
-                    });
-                    let response = httpMocks.createResponse({
-                        eventEmitter: EventEmitter
-                    });
-
-                    function loop(id) {
-                        return borrowData.filter(function(data) {
-                            return data.userId === id;
+            response.on('send', () => {
+                try {
+                    assert.equal(response._getStatusCode(), 201);
+                    assert.equal(response._getData().length, userData.length)
+                    response._getData().map((book) => {
+                        return book.getBooks().then(function(userBooks) {
+                            let userId = book.id;
+                            assert.equal(userBooks.length, loop(userId).length);
+                            done();
                         });
-                    }
-
-                    response.on('send', () => {
-                        try {
-                            assert.equal(response._getStatusCode(), 201);
-                            assert.equal(response._getData().length, userData.length)
-                            response._getData().map((book) => {
-                                return book.getBooks().then(function(userBooks) {
-                                    let userId = book.id;
-                                    assert.equal(userBooks.length, loop(userId).length);
-                                    done();
-                                });
-                            });
-                        } catch (e) { console.log(e); }
                     });
-
-                    createTest();
-
-                    setTimeout(() => {
-                        userController.userBooks(request, response);
-                    }, 10000);
-                }).timeout(30000);
-
-                it('it should retrieve a user book', (done) => {
-                    let request = httpMocks.createRequest({
-                        method: 'GET',
-                        params: { userId: 1 },
-                        query: { returned: false },
-                        body: {}
-
-                    });
-                    let response = httpMocks.createResponse({
-                        eventEmitter: EventEmitter
-                    });
-
-                    response.on('send', () => {
-                        try {
-                            assert.equal(response._getStatusCode(), 201);
-                            assert.equal(response._getData().id, 1); // ensure user 1 is returned
-                            response._getData().getBooks().then(books => {
-                                assert.equal(books.length, 2); // return two books unreturned by user
-                                done();
-                            });
-                        } catch (e) { console.log(e); }
-                    });
-                    userController.retrieveOne(request, response);
-                });
+                } catch (e) { console.log(e); }
             });
+
+            createTest();
+
+            setTimeout(() => {
+                userController.userBooks(request, response);
+            }, 10000);
+        }).timeout(30000);
+
+        it('it should retrieve a user book', (done) => {
+            let request = httpMocks.createRequest({
+                method: 'GET',
+                params: { userId: 1 },
+                query: { returned: false },
+                body: {}
+
+            });
+            let response = httpMocks.createResponse({
+                eventEmitter: EventEmitter
+            });
+
+            response.on('send', () => {
+                try {
+                    assert.equal(response._getStatusCode(), 201);
+                    assert.equal(response._getData().id, 1); // ensure user 1 is returned
+                    response._getData().getBooks().then(books => {
+                        assert.equal(books.length, 2); // return two books unreturned by user
+                        done();
+                    });
+                } catch (e) { console.log(e); }
+            });
+            userController.retrieveOne(request, response);
+        });
+    });
+});

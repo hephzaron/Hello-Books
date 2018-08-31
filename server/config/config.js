acf5459e371e2b711caf7215e1e0475dd7ebf05c
tobi.dara <tobi_daramola@yahoo.com>
2018-08-31 08:43:33 +0100

    script passes all test

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,26 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        username: process.env.DEV_USER || 'postgres',
-        password: process.env.DEV_PASSWORD || 1234,
-        database: 'library',
-        host: '127.0.0.1',
-        port: process.env.DEV_PORT || 8000,
-        dialect: 'postgres'
+  development: {
+    username: process.env.DEV_USER || 'postgres',
+    password: process.env.DEV_PASSWORD || 1234,
+    database: 'testdb',
+    host: '127.0.0.1',
+    port: process.env.DEV_PORT || 8000,
+    dialect: 'postgres'
 
-    },
-    test: {
-        username: process.env.TEST_USER || 'postgres',
-        password: process.env.TEST_PASSWORD || 1234,
-        database: 'testdb',
-        host: '127.0.0.1',
-        port: process.env.TEST_PORT || 8000,
-        dialect: 'postgres'
-    },
-    production: {
-        url: process.env.DATABASE_URL,
-        dialect: 'postgres',
-    }
+  },
+  test: {
+    username: process.env.TEST_USER || 'postgres',
+    password: process.env.TEST_PASSWORD || 1234,
+    database: 'testdb',
+    host: '127.0.0.1',
+    port: process.env.TEST_PORT || 8000,
+    dialect: 'postgres'
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  }
 
 }
--- server/middlewares/userSignUp.js
+++ server/middlewares/userSignUp.js
@@ -3,48 +3,50 @@
 const LocalUsers = require('../models').LocalUser;
 
 module.exports = {
-    signUp: function(req, res, next) {
+  signUp: function(req, res, next) {
 
-        return LocalUsers.find({
+    return LocalUsers.find({
+      where: {
+        username: req.body.username
+      }
+    }).then(
+      user => {
+        console.log(1);
+        if (!user) {
+          console.log(2);
+          return LocalUsers.find({
             where: {
-                username: req.body.username
+              email: req.body.email
             }
-        }).then(
-            user => {
-                if (!user) {
-                    return LocalUsers.find({
-                        where: {
-                            email: req.body.email
-                        }
-                    }).then(userEmail => {
-                        if (!userEmail) {
-                            // if any user field is empty throw error
-                            const {
-                                username,
-                                email,
-                                password,
-                                confirmPassword
-                            } = req.body;
-                            if (username === (null || '') || email === (null || '') || password === (null || '') || confirmPassword === (null || '')) {
-                                res.status(400).send({ message: 'Some fields are empty' });
-                            } else if (password !== confirmPassword) {
-                                res.status(400).send({ message: 'Password does not match' });
-                            } else {
-                                next();
-                            }
-                        }
-                        if (userEmail) {
-                            res.status(409).send({ message: 'This email is registered' });
-                        }
-                    }).catch(() => res.status(500).send({ message: 'Internal Server Error' }));
-
-                }
-                if (user) {
-                    res.status(409).send({ message: 'username already exist' });
-                }
+          }).then(userEmail => {
+            if (!userEmail) {
+              // if any user field is empty throw error
+              const {
+                username,
+                email,
+                password,
+                confirmPassword
+              } = req.body;
+              if (username === (null || '') || email === (null || '') || password === (null || '') || confirmPassword === (null || '')) {
+                res.status(400).send({ message: 'Some fields are empty' });
+              } else if (password !== confirmPassword) {
+                res.status(400).send({ message: 'Password does not match' });
+              } else {
+                next();
+              }
             }
-        ).catch((errors) => res.status(500).send({ message: 'Internal Server Error', errors }));
+            if (userEmail) {
+              res.status(409).send({ message: 'This email is registered' });
+            }
+          }).catch(() => res.status(500).send({ message: 'Internal Server Error' }));
 
-    }
+        }
+        if (user) {
+          res.status(409).send({ message: 'username already exist' });
+        }
+      }
+    ).catch((errors) => res.status(500).send({ message: 'Internal Server Error-1', errors }));
+
+  }
 
 };
